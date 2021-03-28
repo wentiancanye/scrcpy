df315fcbffff1ae64612b28dc1b13ea1c78820eb
Canye <1443854929@qq.com>
2021-03-28 09:33:32 +0800

    坑了我一脸鼻血
    编译之前应该先编译依赖的FFmpeg和sdl,不然编译不成功

--- buildapp.sh
+++ buildapp.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+cd prebuilt-deps/
+make prepare-win64
+cd ..
 meson x --buildtype release --strip -Db_lto=true -Dcrossbuild_windows=true --cross-file=cross_win64.txt -Dcompile_server=false -Dcompile_app=true -Dportable=true
 ninja -Cx
