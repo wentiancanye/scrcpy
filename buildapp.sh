2a679323809cc8b115e4d085dfd0a44c0351d273
1443854929 <1443854929@qq.com>
2022-01-30 06:13:58 -0800

    简化编译流程

--- buildapp.sh
+++ buildapp.sh
@@ -3,8 +3,15 @@
 # 也可以根据cross_win64.txt和cross_win64.txt中的properties节中需要的版本进行下载,放入prebuilt-deps文件夹中
 # 但是现在(2021-03-28)ffmpeg已经不再提供32位版本,并且dev和shared已经合并,可以直接下载shared版本,将其拷贝为deV
 # 但是当只有当prepare-win64失效时才需要手动下载
-cd prebuilt-deps/
-make prepare-win64
-cd ..
-meson x --buildtype release --strip -Db_lto=true -Dcrossbuild_windows=true --cross-file=cross_win64.txt -Dcompile_server=false -Dcompile_app=true -Dportable=true
-ninja -Cx
+
+#cd prebuilt-deps/
+#make prepare-win64
+#cd ..
+#meson x --buildtype release --strip -Db_lto=true -Dcrossbuild_windows=true --cross-file=cross_win64.txt -Dcompile_server=false -Dcompile_app=true -Dportable=true
+#ninja -Cx
+
+make -f release.mk prepare-deps-win64
+imake -f release.mk build-win64
+cd build-win64/app
+ls -l
+
